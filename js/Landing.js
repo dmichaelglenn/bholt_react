889d78478f1030424f2b9200e7f099f0cabf07ce
Michael Glenn <dmichaelglenn@gmail.com>
2017-08-17 12:47:45 -0700

    added search page

---
+++ js/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Landing = React.createClass({
+  render () {
+    return (
+      <div className='landing'>
+        <h1>svideo</h1>
+        <input type='text' placeholder='Search' />
+        <a>or browse all</a>
+      </div>
+    )
+  }
+
+})
+
+export default Landing
